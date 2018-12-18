16506247bf62e61253ee2b046653d9a332502335
Oleg Kravchenko <okravc@gmail.com>
2018-12-18 21:30:24 +0300

    on style

--- prepare.rb
+++ prepare.rb
@@ -7,39 +7,37 @@ require 'erb'
 
 class Prepare
   def initialize
-    @o = Struct.new(:v, :url, :ip, :log, :data).new
+    optparse
+    @uri = URI.parse @o.url
+    @tpl_lua = ERB.new(File.read('lua.erb'), nil, '-')
+  end
+
+  def go
+    puts @tpl_lua.result_with_hash(
+      port:   @uri.port,
+      host:   @uri.host,
+      scheme: @uri.scheme,
+      path:   @uri.path,
+      query:  @uri.query,
+      ip:     @o.ip,
+      data:   @o.data,
+      log:    @o.log
+    )
+  end
+
+  private
+
+  def optparse
+    @o = Struct.new(:url, :ip, :log, :data).new
     OptionParser.new do |opts|
-      opts.on('-v',      'verbose')   { @o.v        = true }
       opts.on('-u URL',  'url')       { |u| @o.url  = u }
       opts.on('-i IP',   'resolve')   { |i| @o.ip   = i }
       opts.on('-l log',  'logfile')   { |l| @o.log  = l }
       opts.on('-d data', 'post data') { |d| @o.data = d }
       @opts = opts
     end.parse!
-    abort 'url?' unless @o.url
-    @uri = URI.parse @o.url
-    puts "==> #{@uri.scheme}://#{@uri.host}:#{@uri.port}" \
-         "#{@uri.path}?#{@uri.query}"
-
-    tpl = File.read('lua.erb')
-    @tpl_lua = ERB.new(tpl, nil, '-')
+    abort 'give me url with -u' unless @o.url
   end
-
-  def go
-    lua = @tpl_lua.result_with_hash(
-      port:   @uri.port,
-      host:   @uri.host,
-      scheme: @uri.scheme,
-      path:   @uri.path,
-      ip:     @o.ip,
-      data:   @o.data,
-      log:    @o.log
-    )
-    puts lua
-  end
-
-  private
-
 end
 
 Prepare.new.go
